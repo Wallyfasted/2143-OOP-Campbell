93ed7954e7d5239e6d64306cdb0383e1ff9ea1aa
Samuel <samuel99campbell@yahoo.com>
2021-09-16 16:37:02 -0500

    continued

---
+++ Assignments/A03/P01/README.md
@@ -0,0 +1,18 @@
+## P01 -  MyVector Class
+### Samuel Campbell
+### Description:
+
+Makes a My vector class that has a double linked class, and has several functions to transverse and modify the list.
+
+### Files
+
+|   #   | File      | Description                      |
+| :---: | --------- | -------------------------------- |
+|   1   | main.cpp  | Main driver of my list program . |
+|   2   | input.dat | houses input for the list .      |
+
+### Instructions
+
+- This program does not require any non standard libraries
+
+
--- Assignments/A03/P01/main.cpp
+++ Assignments/A03/P01/main.cpp
@@ -1,3 +1,22 @@
+/*****************************************************************************
+*                    
+*  Author:           Samuel Campbell
+*  Email:            Sccampbell1019@my.msutexas.edu
+*  Label:            P01
+*  Title:            MyVector Class
+*  Course:           CMPS 2143
+*  Semester:         Fall 2021
+* 
+*  Description:
+*        Makes a My vector class that has a double linked class, 
+*        and has several functions to transverse and modify the list. 
+* 
+*  Usage:
+*        how to use the program if necessary
+* 
+*  Files:            (list of all source files used in this program)
+*****************************************************************************/
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -16,7 +35,7 @@ struct Node
 
 class MyVector
 {
-private:
+    private:
     Node* head;
     Node* tail;
     int size;
@@ -66,7 +85,12 @@ public:
     }
     //Takes in MyVector and pushes to the front of the list
     void PushFront(MyVector V2){
-
+        int x = V2.PopRear(); 
+        while (x != -1){
+            PushFront(x);
+            x = V2.PopRear();
+            cout<<x;
+        }
     }
     void PushRear(int val){
         Node* Temp =new Node(val);
@@ -92,7 +116,29 @@ public:
 
     }
     //takes in a index and puts at that location in list(use size for this then loop index amount of times then place node there)
-    void PushAt(int index, int val){
+    bool PushAt(int index, int val){
+
+        Node* prev = head;            // get previous and next pointers
+        Node* current = head;
+        Node* Temp = head;
+
+        if (index >= size){
+            return false;
+        }
+
+        while (index > 0){
+            prev = current;
+            current = current -> next;
+            index--;
+        }
+
+        Temp -> next = prev -> next;
+        prev -> next = Temp;
+
+        size ++;
+
+        return true;
+        
         
     }
     int PopFront(){
@@ -108,8 +154,16 @@ public:
         }
     }
     int PopRear(){
-        return -1;
-    }//takes in a index and pops at that location in list(use size for this then loop index amount of times then place node there)
+         if(head == NULL){
+          //cout<<"empty\n";
+            return -1;
+        }else{
+            int value = tail->data;
+           Node* Temp = tail;
+           tail = tail -> next;
+           delete Temp;
+           return value;
+    }}//takes in a index and pops at that location in list(use size for this then loop index amount of times then place node there)
     int PopAt(int index){
         return -1;
     }
@@ -135,16 +189,17 @@ int main()
 {   
     //MyVector test("in1.dat");
     MyVector test;
-    MyVector test2;
-    test2.PushFront(12);
-    test2.PushFront(13);
-    test2.PushFront(14);
-
+  //  MyVector test2;
+  //  test2.PushFront(12);
+   // test2.PushFront(13);
+   // test2.PushFront(14);
+   
     test.PushFront(0);
     test.PushFront(1);
     test.PushRear(10);
     test.PushRear(20);
-    test.PushRear(test2);
+     test.PushAt(2,2);
+   // test.PushFront(test2);
     test.Print();
 
     return 0;
